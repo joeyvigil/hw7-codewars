273f0cb333265075a16f6b8bee87819120ede87a
Joseph Vigil <joeyvigil109329@gmail.com>
2025-07-09 02:25:28 -0600

    Initial Commit

---
+++ codewars2.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+
+def summation(num):
+    total = 0
+    i = 1
+    while i <= num:
+      total=total+i
+      i += 1
+    return total
